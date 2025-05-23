4c035e0589573c494d2a6291a6a6d5755c35159c
ANANTA FIRDAUS <ananta23ti@mahasiswa.pcr.ac.id>
2025-05-23 23:37:03 +0700

    commit 4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-Web-Sedap/', 
+  // base: '/React-Nta/', 
   plugins: [
     react(),
     tailwindcss(),
